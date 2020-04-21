f234b1b2b50aa8fd8982abe3323080ba95d13398
Ismael Ventura <ismael.ventura@ies2.com.br>
2020-04-21 14:27:27 -0300

    Criando funções para acessar whatsapp, email e para voltar página.

--- mobile/src/pages/Details/index.js
+++ mobile/src/pages/Details/index.js
@@ -1,17 +1,38 @@
 import React from 'react';
 import { Feather } from '@expo/vector-icons';
-import { View, Image, Text, TouchableOpacity } from 'react-native';
+import { useNavigation } from '@react-navigation/native';
+import { View, Image, Text, TouchableOpacity, Linking } from 'react-native';
+import * as MailComposer from 'expo-mail-composer';
 
 import logoImg from '../../assets/logo.png';
 import styles from './styles';
 
 export default function Details() {
+  const navigation = useNavigation();
+  const message = 'Texto aqui.'
+
+  function navigateBack() {
+    navigation.goBack();
+  }
+
+  function sendMail() {
+    MailComposer.composeAsync({
+      subject: 'Heroi do caso: Hihi',
+      recipients: ['ismael.venturadasilva0@gmail.com'],
+      body: message
+    })
+  }
+
+  function sendWhatsapp() {
+    Linking.openURL(`whatsapp://send?phone=5519987324661&text=${message}`);
+  }
+
   return(
     <View style={styles.container}>
       <View style={styles.header}>
         <Image source={logoImg} />
 
-        <TouchableOpacity onPress={() => {}}>
+        <TouchableOpacity onPress={navigateBack}>
           <Feather name="arrow-left" size={26} color="#e02041" />
         </TouchableOpacity>
       </View>
@@ -32,11 +53,11 @@ export default function Details() {
         <Text style={styles.heroDescription}>Entre em contato:</Text>
 
         <View style={styles.actions}>
-          <TouchableOpacity style={styles.action} onPress={() => {}}>
+          <TouchableOpacity style={styles.action} onPress={sendWhatsapp}>
             <Text style={styles.actionText}>Whatsapp</Text>
           </TouchableOpacity>
 
-          <TouchableOpacity style={styles.action} onPress={() => {}}>
+          <TouchableOpacity style={styles.action} onPress={sendMail}>
             <Text style={styles.actionText}>E-mail</Text>
           </TouchableOpacity>
         </View>
