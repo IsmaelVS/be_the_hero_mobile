7738f9a7bd58743873cccb28b93668a7c194705e
Ismael Ventura <ismael.ventura@ies2.com.br>
2020-04-21 00:01:19 -0300

    Criação das rotas e utilização das mesmas.

---
+++ mobile/src/routes.js
@@ -0,0 +1,19 @@
+import React from 'react';
+import { NavigationContainer } from '@react-navigation/native';
+import { createStackNavigator } from '@react-navigation/stack';
+
+import Incidents from './pages/Incidents';
+import Details from './pages/Details';
+
+const AppStack = createStackNavigator();
+
+export default function Routes() {
+  return(
+    <NavigationContainer>
+      <AppStack.Navigator screenOptions={{ headerShown: false }} >
+        <AppStack.Screen name="Details" component={Incidents} />
+        <AppStack.Screen name="Incidents" component={Details} />
+      </AppStack.Navigator>
+    </NavigationContainer>
+  );
+}
